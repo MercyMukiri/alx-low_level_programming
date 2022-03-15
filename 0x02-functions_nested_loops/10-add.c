4c457138595262c2a8ab6566106d632ced88e5a8
MercyMukiri <mwmukiri@gmail.com>
2022-03-15 11:04:58 -0700

    Add file 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * @i: parameter for the function add
+ * @j: parameter for the function add
+ * Return: r
+*/
+
+int add(int i, int j)
+{
+	int r;
+
+	r = i + j;
+
+	return (r);
+}
