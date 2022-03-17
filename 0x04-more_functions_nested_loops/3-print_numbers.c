8c262ce908fbb2a542fc9b65709e6061b025d5ac
MercyMukiri <mwmukiri@gmail.com>
2022-03-17 00:42:42 -0700

    Add file 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbersfrom 0 to 9, followed by a new line
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+		_putchar(c);
+
+	_putchar('\n');
+}
