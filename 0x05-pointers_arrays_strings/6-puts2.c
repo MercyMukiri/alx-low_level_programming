ee6b8b98c749dc59bca390dedb34af46630e9fd0
MercyMukiri <mwmukiri@gmail.com>
2022-03-21 12:15:32 -0700

    Edit file 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,7 +4,7 @@
  * puts2 - prints every other character of a string,
  * starting with the first character, followed by a new line.
  *
- * @str - parameter for the puts2 function
+ * @str: parameter for the puts2 function
  *
  * Return : void
  */
