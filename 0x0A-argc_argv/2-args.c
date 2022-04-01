95a3f45e6307cae83fb4d41599fbc9f7aa34d998
MercyMukiri <mwmukiri@gmail.com>
2022-04-01 07:59:13 -0700

    Edit file 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - prints the program name, followed by a new line
+ * main - prints all arguments it receives
  *
  * @argc: number of arguments provided in the command line
  * @argv: an array of pointers to the strings which are those arguments
