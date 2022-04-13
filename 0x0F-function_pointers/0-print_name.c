521a2ab96a3b5eaf995ab2eb28af3481e4785890
MercyMukiri <mwmukiri@gmail.com>
2022-04-13 02:50:22 -0700

    Add README.md, function_pointers.h and 0-print_name.c files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ *
+ * @name: name of the person
+ * @f: a function pointer
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
