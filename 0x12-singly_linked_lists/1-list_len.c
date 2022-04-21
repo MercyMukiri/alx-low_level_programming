9bca072f07c5269daf2d93530922f2f2a3604e19
MercyMukiri <mwmukiri@gmail.com>
2022-04-21 03:03:21 -0700

    Add file 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - prints the number of elements in a linked list_t list
+ *
+ * @h: singly linked list
+ *
+ * Return: number of elements in a linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		h = h->next;
+		size++;
+	}
+
+	return (size);
+}
