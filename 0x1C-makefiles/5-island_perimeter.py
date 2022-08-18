0dfe7d749f78bcc471e233d24e6a40133bb718a5
MercyMukiri <mwmukiri@gmail.com>
2022-08-17 19:50:24 -0700

    Update file 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,30 +2,28 @@
 """ This module returns the perimeter of the island decribed in grid"""
 
 
-def get_surrounding_water(grid, i, j):
-    """Ths function returns the number of sides surrounded by water"""
-    water_sides = 0
-
-    if not grid[i][j + 1] or j >= len(grid[i]) - 1:
-        water_sides += 1
-    if not grid[i - 1][j] or i <= 0:
-        water_sides += 1
-    if not grid[i][j - 1] or j <= 0:
-        water_sides += 1
-    if not grid[i + 1][j] or i >= len(grid) - 1:
-        water_sides += 1
-
-    return water_sides
-
-
 def island_perimeter(grid):
-    """ This function returns the perimeter of the island described in grid"""
+    """
+    This function returns the perimeter of the island described in grid
 
-    perimeter = 0
+    Args:
+        grid (list): A list of list of integers representing an island.
+
+    Return:
+        The perimeter of the island
+    """
+
+    land = 0
+    edge = 0
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j]:
-                perimeter += get_surrounding_water(grid, i, j)
+            if grid[i][j] == 1:
+                land += 1
 
-    return perimeter
+                if grid[i - 1][j] == 1 and i > 0:
+                    edge += 1
+                if grid[i][j - 1] == 1 and j > 0:
+                    edge += 1
+
+    return land * 4 - edge * 2
