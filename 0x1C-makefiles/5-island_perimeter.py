79a11d757f0f8db92288b4fb023fa1578cb2574c
MercyMukiri <mwmukiri@gmail.com>
2022-08-17 19:25:27 -0700

    Update file 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,6 +2,22 @@
 """ This module returns the perimeter of the island decribed in grid"""
 
 
+def get_surrounding_water(grid, i, j):
+    """Ths function returns the number of sides surrounded by water"""
+    water_sides = 0
+
+    if not grid[i][j + 1] or j >= len(grid[i]) - 1:
+        water_sides += 1
+    if not grid[i - 1][j] or i <= 0:
+        water_sides += 1
+    if not grid[i][j - 1] or j <= 0:
+        water_sides += 1
+    if not grid[i + 1][j] or i >= len(grid) - 1:
+        water_sides += 1
+
+    return water_sides
+
+
 def island_perimeter(grid):
     """ This function returns the perimeter of the island described in grid"""
 
@@ -13,19 +29,3 @@ def island_perimeter(grid):
                 perimeter += get_surrounding_water(grid, i, j)
 
     return perimeter
-
-
-def get_surrounding_water(grid, i, j):
-    """Ths function returns the number of sides surrounded by water"""
-    water_sides = 0
-
-    if grid[i][j + 1] == 0 or j >= len(grid[i]) - 1:
-        water_sides += 1
-    if grid[i - 1][j] == 0 or i <= 0:
-        water_sides += 1
-    if grid[i][j - 1] == 0 or j <= 0:
-        water_sides += 1
-    if grid[i + 1][j] == 0 or i >= len(grid) - 1:
-        water_sides += 1
-
-    return water_sides
